ad93a85094654b8617159eb6d13b43ee8e4b53ce
Frank <f.iossa@outlook.com>
2025-07-24 23:09:17 +0200

    .

--- components/samsung/samsung.cpp
+++ components/samsung/samsung.cpp
@@ -16,7 +16,7 @@ namespace esphome
         {
             climate_ir::ClimateIR::setup();
             this->apply_state();
-            LOG_PIN("  Pin: ", this->transmitter_.pin_);
+            LOG_PIN("  Pin: ", this->transmitter_->pin_);
         }
 
         climate::ClimateTraits SamsungClimate::traits()
