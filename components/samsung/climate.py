74a71231d86e5daf3852dabc5b9e33990dbe4f8c
Frank <f.iossa@outlook.com>
2025-07-25 09:39:29 +0200

    added loaded IRremoteESP8266

--- components/samsung/climate.py
+++ components/samsung/climate.py
@@ -1,10 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
+from esphome.components import ir_remote_base
 from esphome.const import CONF_PIN
 from esphome import pins
 
-AUTO_LOAD = ["climate_ir"]
+AUTO_LOAD = ["climate_ir", "ir_remote_base"]
 
 samsung_ns = cg.esphome_ns.namespace("samsung")
 samsungClimate = samsung_ns.class_("SamsungClimate", climate_ir.ClimateIR)
@@ -17,6 +18,7 @@ CONFIG_SCHEMA = climate_ir.climate_ir_schema(samsungClimate).extend(
 
 
 async def to_code(config):   
+    ir_remote_base.load_ir_remote()
     var = await climate_ir.new_climate_ir(config)
     #cg.add(var.set_model(config[CONF_MODEL]))
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
